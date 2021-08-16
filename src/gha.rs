2a5fd941ab266d8b850d47fcb4f3d4be10315ec9
S.Percentage <Syn.Tri.Naga@gmail.com>
2021-08-16 16:34:29 +0900

    fix: use cargo_metadata

--- src/gha.rs
+++ src/gha.rs
@@ -1,1 +1,103 @@
-pub mod annotation;
+use cargo_metadata::diagnostic::{Diagnostic, DiagnosticLevel};
+use cargo_metadata::{CompilerMessage, Message};
+use std::convert::TryFrom;
+use std::path::PathBuf;
+
+pub enum WorkflowCommand {
+    Warning {
+        file: PathBuf,
+        line: usize,
+        col: usize,
+        message: String,
+    },
+    Error {
+        file: PathBuf,
+        line: usize,
+        col: usize,
+        message: String,
+    },
+}
+impl std::fmt::Display for WorkflowCommand {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Self::Warning {
+                file,
+                line,
+                col,
+                message,
+            } => write!(
+                fmt,
+                "::warning file={},line={},col={}::{}",
+                file.display(),
+                line,
+                col,
+                message
+            ),
+            Self::Error {
+                file,
+                line,
+                col,
+                message,
+            } => write!(
+                fmt,
+                "::error file={},line={},col={}::{}",
+                file.display(),
+                line,
+                col,
+                message
+            ),
+        }
+    }
+}
+
+impl TryFrom<Message> for WorkflowCommand {
+    type Error = ();
+
+    fn try_from(base: Message) -> Result<Self, ()> {
+        match base {
+            Message::CompilerMessage(CompilerMessage {
+                message:
+                    Diagnostic {
+                        level: DiagnosticLevel::Warning,
+                        message,
+                        mut spans,
+                        ..
+                    },
+                ..
+            }) if !spans.is_empty() => {
+                let span = spans.pop().unwrap();
+
+                Ok(Self::Warning {
+                    file: std::env::current_dir()
+                        .expect("Failed to get current dir")
+                        .join(span.file_name),
+                    line: span.line_start,
+                    col: span.column_start,
+                    message,
+                })
+            }
+            Message::CompilerMessage(CompilerMessage {
+                message:
+                    Diagnostic {
+                        level: DiagnosticLevel::Error,
+                        message,
+                        mut spans,
+                        ..
+                    },
+                ..
+            }) if !spans.is_empty() => {
+                let span = spans.pop().unwrap();
+
+                Ok(Self::Error {
+                    file: std::env::current_dir()
+                        .expect("Failed to get current dir")
+                        .join(span.file_name),
+                    line: span.line_start,
+                    col: span.column_start,
+                    message,
+                })
+            }
+            _ => Err(()),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,12 @@
-use std::{convert::TryFrom, io::BufRead};
+use std::convert::TryFrom;
 
-mod cargo;
 mod gha;
 
 fn main() {
-    let input = std::io::stdin();
-    let input_lines = input.lock().lines().filter_map(|l| {
-        l.map(|l| {
-            serde_json::from_str::<cargo::jsonfmt::BuildEngineMessage>(&l)
-                .ok()
-                .and_then(|e| gha::annotation::WorkflowCommand::try_from(e).ok())
-        })
-        .transpose()
-    });
-
-    let mut found_error = false;
-    for cmd in input_lines {
-        let cmd = cmd.expect("Failed to read stdin");
-        found_error = found_error || cmd.is_error();
+    let stdin = std::io::stdin();
+    let stream = cargo_metadata::Message::parse_stream(stdin.lock())
+        .filter_map(|msg| gha::WorkflowCommand::try_from(msg.expect("Failed to read stdin")).ok());
+    for cmd in stream {
         println!("{}", cmd);
     }
-
-    std::process::exit(if found_error { 101 } else { 0 });
 }
